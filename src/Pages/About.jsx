fc38adcf44917a7817e7f7e3c2118b5c3aaf49f1
Ddiiro <Ddiiro@ddiiro.local>
2024-08-18 13:36:09 +0300

    Setup the about page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -2,7 +2,38 @@ import React from 'react'
 
 function About() {
   return (
-    <div>About</div>
+    <>
+       <div className='flex mt-1 bg-blue-950 w-full'>
+        <div className='w-2/6 p-11'>
+          <h1 className='flex text-l w-2/6 m-auto mb-4'>About Us</h1>
+          <p className='text-sm'>
+          Lorem Ipsum is simply dummy text of the printing and 
+          typesetting industry. Lorem Ipsum has been the industry's 
+          standard dummy text ever since the 1500s, when an unknown printer 
+          took a galley of type and scrambled it to make a type specimen book. 
+          It has survived not only five centuries, but also the leap into electronic typesetting, 
+          remaining essentially unchanged. It was popularised in the 1960s with the release of 
+          Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing 
+          software like Aldus PageMaker including versions of Lorem Ipsum.
+          </p>
+          <p className='text-sm'>
+          Lorem Ipsum is simply dummy text of the printing and 
+          typesetting industry. Lorem Ipsum has been the industry's 
+          standard dummy text ever since the 1500s, when an unknown printer 
+          took a galley of type and scrambled it to make a type specimen book. 
+          It has survived not only five centuries, but also the leap into electronic typesetting, 
+          remaining essentially unchanged. It was popularised in the 1960s with the release of 
+          Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing 
+          software like Aldus PageMaker including versions of Lorem Ipsum.
+          It has survived not only five centuries, but also the leap into electronic typesetting, 
+          remaining essentially unchanged. It was popularised in the 1960s with the release.
+          </p>
+        </div>
+        <div className='w-4/6'>
+            <img className='w-full p-2 h-full' src="/src/assets/store.jpg" alt="" />
+        </div>
+       </div>
+    </>
   )
 }
 
--- src/components/generic/Header.jsx
+++ src/components/generic/Header.jsx
@@ -4,7 +4,7 @@ import NavBar from './NavBar'
 import { Link } from 'react-router-dom'
 function Nav() {
   return (
-    <div className='flex bg-blue-950 place-content-between'>
+    <div className='flex bg-blue-950 place-content-between rounded-xl'>
       <Link to="/"><Logo className='w-1/6'/></Link>
       
       <NavBar className='w-5/6'/>
